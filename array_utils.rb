7e882e777e51f6ed6eda0a9e88a8791c40f68059
Cecilia Marcora <cecilia.marcora@moove-it.com>
2015-09-14 18:52:47 -0300

    Soluciones errores de sintaxis y utilize el unless

--- array_utils.rb
+++ array_utils.rb
@@ -1,9 +1,9 @@
 module ArrayUtils
-	def self.arraty_max(param_array)
+	def self.array_max(param_array)
 		max= param_array
 
 		param_array.each do |element|
-			max= element if element > max { |a, b| edn
+			max= element unless element > max { |a, b| end
 		end
 	max 
 	end
